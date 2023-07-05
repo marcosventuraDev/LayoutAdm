53574de8ec2719b5b89731efe0f4315c7c6153c6
marcos ventura <marcosventura.dev@gmail.com>
2023-07-05 01:18:30 -0300

    dropdown concluido com sucesso

--- js/custom_adm.js
+++ js/custom_adm.js
@@ -1,82 +1,41 @@
-// Dropdown navbar
 
-let item_1 = document.querySelector("#item-1");
-let item_2 = document.querySelector("#item-2");
-dropMenu( item_1, 1);
-dropMenu( item_2, 2); 
-let select= null;
-let indice = null; 
-let clicked = null;
-function dropMenu(selector, indice){
+ // Dropdown navbar
 
-  selector.addEventListener("click",()=>{
-    if(!document.querySelector(".toggle").classList.contains('show')){
-      document.querySelector(".toggle").classList.add('show')
-    }
-    clicked = document.querySelector(`#item-${indice}`);
-    indice = indice
-    indice < 2 ? select =`item-${indice+1}`:select =`item-${indice-1}`
-    value = document.querySelector("#"+select)
-    result =  value.checked = false;
-   
-   
+// obtém os elementos dos menus suspensos
+let drop_notification = document.querySelector(".drop_notification");
+let drop_avatar = document.querySelector(".drop_avatar");
 
-      },
-     
-     );
-   
- 
-   }
- 
-   window.onclick = function(event) {
-    if (!event.target.matches('.toggle')) {
-      var dropdowns = document.getElementsByClassName("toggle");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        
-        var openDropdown = dropdowns[i];
-        
-if(openDropdown.classList.contains('show')) {
-  if(clicked!= null && openDropdown.classList.contains('show')){
-    clicked.checked = false
-   
-  }
-  openDropdown.classList.remove('show');
-          console.log(clicked)
-        }else{
-          openDropdown.classList.add('show');
-        }
-        
-     
-      }
-       
-      
-    }
-  }
 
+// obtér os botões que abrem os menus suspensos
+let button_notification = document.querySelector(".button_notification");
+let button_avatar = document.querySelector(".button_avatar");
+
+dropMenu(button_notification, drop_notification);
+dropMenu(button_avatar, drop_avatar);
+
+function dropMenu(button, dropdown){
+  button.addEventListener('click',()=>{
+
+    dropdown == drop_notification && drop_avatar.classList.contains('show')?
+    drop_avatar.classList.remove('show'):
+    drop_notification.classList.remove('show')
+
+    // adiciona um evento de clique ao botão para abrir o menu suspenso
     
-  /*  window.onclick = function(event) {
-    var pai = document.getElementById("noPai"); // o nó pai da div
-    var filhos = pai.children; // todos os elementos filhos do nó pai
-    var clicouFora = true; // uma variável para indicar se o clique foi fora dos filhos
-    for (var i = 0; i < filhos.length; i++) { // percorre todos os filhos
-      if (filhos[i].contains(event.target)) { // verifica se o filho contém o elemento clicado
-        clicouFora = null; // se sim, muda a variável para false
-          console.log(filhos[i])
-        break; // sai do loop
-      
+    button.onclick = function() {
+      dropdown.classList.toggle("show");
+    }
+
+    // adiciona um evento de clique à janela para fechar o menu suspenso
+    window.onclick = function(event) {
+      // verifica se o usuário clicou fora do menu suspenso e do botão
+      if (event.target != dropdown && event.target != button) {
+        // fecha o menu suspenso
+      dropdown.classList.remove('show')
       }
-    }
-    if (clicouFora) { // se a variável ainda for true, significa que o clique foi fora dos filhos
-      console.log("Você clicou fora das áreas excluídas");
-    }
-  }; */
-     
+    } 
+   
+  })
   
 
-
-
-  
-  
-  
- 
+}
