d0a4e218bf091bf4f39d938a6fb9c04dcc6711c8
marcos ventura <marcosventura.dev@gmail.com>
2023-07-06 09:05:45 -0300

    finalizando o js do sidebar

--- js/custom_adm.js
+++ js/custom_adm.js
@@ -4,8 +4,6 @@
 // obtém os elementos dos menus suspensos
 let drop_notification = document.querySelector(".drop_notification");
 let drop_avatar = document.querySelector(".drop_avatar");
-
-
 // obtér os botões que abrem os menus suspensos
 let button_notification = document.querySelector(".button_notification");
 let button_avatar = document.querySelector(".button_avatar");
@@ -15,22 +13,19 @@ dropMenu(button_avatar, drop_avatar);
 
 function dropMenu(button, dropdown){
   button.addEventListener('click',()=>{
-
+//
     switch (dropdown) {
-      case drop_notification:drop_avatar.classList.remove('show')
+      case drop_notification:drop_avatar.classList.remove('show');
         
         break;
     
-      case  drop_avatar:drop_notification.classList.remove('show')
+      case  drop_avatar:drop_notification.classList.remove('show');
         break;
     }
-
     // adiciona um evento de clique ao botão para abrir o menu suspenso
-    
     button.onclick = function() {
       dropdown.classList.toggle("show");
     }
-
     // adiciona um evento de clique à janela para fechar o menu suspenso
     window.onclick = function(event) {
       // verifica se o usuário clicou fora do menu suspenso e do botão
@@ -44,3 +39,14 @@ function dropMenu(button, dropdown){
   
 
 }
+// Fim Dropdown navbar
+
+//Sidebar toogel/ bars
+
+let sidebar = document.querySelector('.sidebar');
+let bars = document.querySelector('.bars');
+
+
+bars.addEventListener('click', ()=>{
+  sidebar.classList.toggle("active");
+})
