fe35411d06e8a377117ca21ee2ebc5956b1f2021
joao marcos <joaomarcosv711@gmail.com>
2023-07-09 18:51:29 -0300

    finalizando o dropdown

--- js/custom_adm.js
+++ js/custom_adm.js
@@ -1,69 +1,37 @@
 
  // Dropdown navbar
 
-// obtém os elementos dos menus suspensos
-let drop_notification = document.querySelector(".drop_notification");
-let drop_avatar = document.querySelector(".drop_avatar");
-let actionDropdown = document.querySelector(".actionDropdown");
-
-// obtér os botões que abrem os menus suspensos
-let button_notification = document.querySelector(".button_notification");
-let button_avatar = document.querySelector(".button_avatar");
-let button_drop_action = document.querySelector(".dropdown-btn-action");
-
-let dorpsdowns = document.querySelector(".navbar-menu").children;//
-let list = document.querySelector(".list-body");
-
-dropMenu(button_notification, drop_notification, dorpsdowns);
-dropMenu(button_avatar, drop_avatar, dorpsdowns);
-dropMenu(button_drop_action, actionDropdown, dorpsdowns);
-
-
-
-
-
-function dropMenu(button, dropdown, camada){
+ 
+ function btn_dropdown(id){
+  closeDropdownAction() 
+  document.querySelector(".drop_"+id).classList.toggle('show');
   
-  button.addEventListener('click',()=>{
+ }
 
-closeDropdownAction()
-    if( dropdown == drop_notification){
-      drop_avatar.classList.remove('show');
-      actionDropdown.classList.remove('show');
-    }else if(dropdown ==  drop_avatar){
-      drop_notification.classList.remove('show');
-      actionDropdown.classList.remove('show');
+ 
 
-    }
-      
-    // adiciona um evento de clique ao botão para abrir o menu suspenso
-    button.onclick = function() {
-      dropdown.classList.toggle("show");
-    }
 
-    // adiciona um evento de clique à janela para fechar o menu suspenso
-    window.onclick = function(event) {
-      // verifica se o usuário clicou fora do menu suspenso e do botão
-      event.target != dropdown && event.target != button?
-        // fecha o menu suspenso
-      dropdown.classList.remove('show'):""
-      
-    } 
-    function closeDropdownAction(){
-      let i;
-      for(i=0; i<camada.length;i++)
-        var openDropdown = camada[i];
-        if(openDropdown.classList.contains("show")){
-          openDropdown.classList.remove("show")
-        }
-    }
-  
-  })
+ window.onclick = function(event){
+   if(!event.target.matches(".dropdown-btn-action")){
+    closeDropdownAction()
+   }
+ }
 
-  
-
-}
-// Fim Dropdown navbar
+  function  closeDropdownAction(){
+    var dropdowns = document.getElementsByClassName("actionDropdown");
+   var i;
+   console.log(dropdowns.length)
+   
+   for (i = 0; i < dropdowns.length; i++){
+     var openDropdown = dropdowns[i];
+    
+     if(openDropdown.classList.contains("show")){
+       openDropdown.classList.remove("show");
+     }
+   }
+ } 
+ 
+// Fim Dropdown navbar */
 
 //Sidebar toogel/ bars
 
